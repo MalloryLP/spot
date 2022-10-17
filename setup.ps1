aa3a00cc15856f1c2e6aee6e5a25549ee6222ecd
Mallory <87539391+MalloryLP@users.noreply.github.com>
2022-10-17 11:05:17 +0200

    Add files via upload

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,3 @@
-C:\Users\mlehu\AppData\Local\Programs\Python\Python310\python.exe -m venv .venv
+C:\Users\mallory.lehuaultparc\AppData\Local\Programs\Python\Python310\python.exe -m venv .venv
 . .venv/Scripts/activate.ps1
 pip install -r requirements.txt
